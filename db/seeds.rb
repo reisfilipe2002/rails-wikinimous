ba81a8d76d25127ec47de3a2937d213fd7325cb4
filipe reis <reisfilipe102@gmail.com>
2023-05-23 17:26:17 -0300

    Created a Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  article = Article.new(
+    title: Faker::TvShows::HowIMetYourMother.catch_phrase,
+    content: Faker::Travel::Airport.name(size: 'large', region: 'united_states')
+  )
+  article.save!
+end
